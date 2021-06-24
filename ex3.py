f5a6bfb88d963fa04bbe9b969a4e5b178042c46a
Gil <elbazg123@gmail.com>
2021-06-24 14:54:19 +0300

    update

--- ex3.py
+++ ex3.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 N = 10
-nb_patterns = 10
+nb_patterns = 1
 
 
 def zero():
@@ -10,15 +10,20 @@ def zero():
         matrices_buffer_list = matrices_file.buffer.read().decode().split("\r\n")
     a = [[int(ch) for ch in element] for element in [matrix for matrix in matrices_buffer_list]]
     matrices = np.array(a)
-    return matrices[0]
+    return matrices
 
 
 def show_matrix(num_list):
-    plt.imshow()
+    plt.imshow(num_list)
+    plt.show()
+
+
+def convert_to_matrix(matrix):
+    new_matrix = matrix.reshape(N, N)
+    return new_matrix
 
 
 if __name__ == '__main__':
-    zero()
-    pass
-    # first = zero()
-    # print(first[0])
+    zero_matrix = zero()
+    test = convert_to_matrix(zero_matrix[0])
+    show_matrix(test)
